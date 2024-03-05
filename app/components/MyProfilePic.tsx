7f8e4e7e2f6d98bdab60c391eb19dbcd4eca571a
Samridda Devkota <samriddadevkota@gmial.com>
2024-03-05 18:21:07 +0545

    first commit

---
+++ app/components/MyProfilePic.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function MyProfilePic() {
+  return (
+    <section className=" w-full mx-auto">
+      <Image
+        src="/images/profile-photo.jpg"
+        width={200}
+        height={200}
+        alt="Samridda Devkota"
+        priority={true}
+        className="border-4 border-black dark:border-slate-500 drop-shadow-xl shadow-black rounded-full mx-auto mt-8"
+      />
+    </section>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
+import Posts from "./components/Posts";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="px-6 mx-auto">
+      <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
+        Hello and Welcome 👋&nbsp;
+        <span className="whitespace-nowrap">
+          I&apos;m <span className="font-bold">Sam</span>.
+        </span>
+      </p>
+      <Posts />
+    </main>
   );
 }
